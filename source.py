502aae5157bab2a14a249bcc46401485f4875b24
dmitrydi <d.dmitrook@gmail.com>
2019-04-02 18:04:59 +0400

    first commit

---
+++ source.py
@@ -0,0 +1,33 @@
+class Source():
+# class for a finite element (finite liquid source) of a well or fracture
+# all in laplace space
+	def __init__(self,
+		outer_bound,
+		top_bound,
+		bottom_bound,
+		type,
+		size,
+		xc,
+		yc,
+		u,
+		params={},
+		zc=0):
+		self.q = None
+		self.outer_bound = outer_bound
+		self.top_bound = top_bound
+		pass
+
+	def G(x,y,z):
+		# Green's function of the source (response in coordinate point (x,y,z)), depends on type and boundaries
+		pass
+
+	def pd(x,y,z=0):
+		# pressure response in space point (x,y,z)
+		# works only if source strength is calculated
+		if self.q is None:
+			raise ValueError("Source strength q in unindentified")
+		pass
+
+
+
+
