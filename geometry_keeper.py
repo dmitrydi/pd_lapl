51bc801093889915ffae293468bb9ef83e4d3eda
dmitrydi <d.dmitrook@gmail.com>
2019-05-15 19:00:14 +0400

    fb3 for nnnn implemented

--- geometry_keeper.py
+++ geometry_keeper.py
@@ -4,14 +4,6 @@ class GeometryKeeper:
 	# works with Laplwell class
 	def __init__(self):
 		pass
-		#self.well = well
-		#self.make_fracture_geometry(self.sg, self.well.xwd, self.well.ywd, self.well.xwd, self.well.ywd, self.well.params["nseg"], self.well.c, self.well.round_val)
-
-	#def make_mutual_geometry(self, gdict, that_well):
-	#	N = this_well.params["nseg"]
-	#	c = this_well.c
-	#	ndigits = this_well.round_val
-	#	self.make_fracture_geometry(gdict, self.well.xwd, self.well.ywd, that_well.xwd, that_well.ywd, N, c, ndigits)
 
 	def make_fracture_geometry(self, well, xd, yd):
 		N = well.params["nseg"]
@@ -25,8 +17,10 @@ class GeometryKeeper:
 		m_xj1 = dx*np.matrix(np.ones(2*N, dtype=np.float)).transpose()*np.matrix(np.arange(-N, N), dtype=np.float)
 		m_xj2 = dx + m_xj1
 		# values for iti0k0 - integration limits are combined here with the point of integration - xi
-		gdict["mxd"] = m_xd
-		gdict["myd"] = np.abs(np.matrix((yd - ywd)*np.ones_like(m_xd)))
+		gdict["mxd"] = np.array(m_xd)
+		gdict["mxj"] = np.array(m_xj1 + xwd)
+		gdict["mxj1"] = np.array(m_xj2 + xwd)
+		gdict["myd"] = np.array(np.abs(np.matrix((yd - ywd)*np.ones_like(m_xd))))
 		gdict["lims1"] = np.round(m_xd - c*m_xj1 - xwd, decimals=ndigits) # lower limits of integration within fracture: lims1[i, j] = dx*(i+0.5) - dx*(j-1)
 		gdict["lims2"] = np.round(m_xd - c*m_xj2 - xwd, decimals=ndigits) # upper limits of integration within fracture: lims2[i, j] = dx*(i+0.5) - dx*j
 		gdict["mask_int_1"] = np.matrix(np.ones((2*N, 2*N)), dtype=np.float) - 2*(gdict["lims1"] < 0) # mask for selecting sign for integrals of lims1 and lims 2
--- source.py
+++ source.py
@@ -19,22 +19,75 @@ class LaplSource():
 		su = (s*fs)**0.5
 		return 0.5*k0(su*((xd - xwd - c*x)**2 + (yd - ywd)**2)**0.5) #check 0.5!
 
-
-	# def calc_source_funcs(self, s):
-	# 	fs = self.f_s(s)
-	# 	su = (s*fs)**0.5
-	# 	c = self.well.c
-	# 	if self.well.wtype == "frac":
-	# 		uvals = 0.5/c/su*iti0k0(su*self.well.gk["ulims"])[1]
-	# 	else:
-	# 		raise NotImplementedError
-	# 	return uvals
-
 	def frac_source_func(self, x, dyd, su):
 		if self.well.outer_bound == "infinite":
 			return 0.5*k0(su*(x*x + dyd*dyd)**0.5)
 		else:
 			raise NotImplementedError
+
+	def integrate_source_functions_bounded(self, s, xds, xjs, xj1s, xed, dyds):
+		return self.ifb1(s, xds, xjs, xj1s, xed, dyds) + self.ifb2(s, xds, xjs, xj1s, xed, dyds) + self.ifb3(s, xds, xjs, xj1s, xed, dyds)
+
+	def ifb3_k(self, s, k, xis, xjs, xj1s, xed, dyds):
+		su = (s*self.f_s(s))**0.5
+		orig_shape = xis.shape
+		fxis, fxjs, fxj1s, fdyds = xis.reshape(-1), xjs.reshape(-1), xj1s.reshape(-1), dyds.reshape(-1)
+		ans = np.zeros_like(fxis)
+		inds_0 = np.argwhere(np.isclose(fdyds, 0.)).flatten()
+		inds_nnz = np.argwhere(fdyds != 0.).flatten()
+		xis_0, xjs_0, xj1s_0 = fxis[inds_0], fxjs[inds_0], fxj1s[inds_0]
+		xis_nnz, xjs_nnz, xj1s_nnz = fxis[inds_nnz], fxjs[inds_nnz], fxj1s[inds_nnz]
+		if len(inds_0) > 0:
+			a1 = su*(xjs_0 + xis_0 - 2*k*xed)
+			b1 = su*(xj1s_0 + xis_0 - 2*k*xed)
+			a2 = su*(xjs_0 - xis_0 - 2*k*xed)
+			b2 = su*(xj1s_0 - xis_0 - 2*k*xed)
+			s1 = 1 - 2*(a1 > 0.)
+			s2 = 1 - 2*(b1 < 0.)
+			s3 = 1 - 2*(a2 > 0.)
+			s4 = 1 - 2*(b2 < 0.)
+			a1 = np.round(np.abs(a1), decimals=6)
+			b1 = np.round(np.abs(b1), decimals=6)
+			a2 = np.round(np.abs(a2), decimals=6)
+			b2 = np.round(np.abs(b2), decimals=6)
+			u1, i1 = np.unique(a1, return_inverse = True)
+			u2, i2 = np.unique(b1, return_inverse = True)
+			u3, i3 = np.unique(a2, return_inverse = True)
+			u4, i4 = np.unique(b2, return_inverse = True)
+			ua = np.concatenate((u1, u2, u3, u4))
+			u, i = np.unique(ua, return_inverse = True)
+			va = 1./su/xed*iti0k0(u)[1][i]
+			v1 = va[:len(u1)][i1]
+			v2 = va[len(u1):len(u1)+len(u2)][i2]
+			v3 = va[len(u1)+len(u2):len(u1)+len(u2)+len(u3)][i3]
+			v4 = va[len(u1)+len(u2)+len(u3):][i4]
+			ans[inds_0] = v1*s1 + v2*s2 + v3*s3 + v4*s4
+		if len(inds_nnz) > 0:
+			raise NotImplementedError
+		return ans.reshape(orig_shape)
+
+	def ifb3_1(self, s, xis, xjs, xj1s, xed, dyds, debug=False):
+		KMAX = 100
+		EPS = 1e-12
+		TINY = 1e-20
+		sum_ = self.ifb3_k(s, 0, xis, xjs, xj1s, xed, dyds)
+		for k in range(1, KMAX):
+			d = self.ifb3_k(s, k, xis, xjs, xj1s, xed, dyds)
+			d += self.ifb3_k(s, -k, xis, xjs, xj1s, xed, dyds)
+			sum_ += d
+		if np.max(d)/(np.min(sum_) + TINY) < EPS:
+			if debug:
+				return (0.5*sum_, k)
+			else:
+				return 0.5*sum_
+		raise RuntimeWarning("ifb3_1 did not converge in {} steps".format(KMAX))
+
+	def ifb3_2(self, s, xjs, xj1s, xed, dyds):
+		su = (s*self.f_s(s))**0.5
+		return -0.5*np.pi/xed/su*(xj1s - xjs)*np.exp(-su*dyds)
+
+	def ifb3(self, s, xis, xjs, xj1s, xed, dyds):
+		return self.ifb3_1(s, xis, xjs, xj1s, xed, dyds) + self.ifb3_2(s, xjs, xj1s, xed, dyds)
 
 	def integrate_source_functions(self, s, lims1, lims2, dyds, int_type = "quad", npoints = 5):
 		fs = self.f_s(s)
@@ -43,24 +96,27 @@ class LaplSource():
 		assert len(lims1) == len(lims2) == len(dyds)
 		ans = np.zeros_like(lims1)
 		if self.well.wtype == "frac":
-			dyds_0_inds = np.argwhere(dyds==0.).flatten()
-			dyds_nnz_inds = np.argwhere(dyds!=0.).flatten()
-			# calcculate for zero dyds:
-			if len(dyds_0_inds) > 0:
-				ints_dyd_0_lims1 = 0.5/c/su*iti0k0(su*lims1[dyds_0_inds])[1]
-				ints_dyd_0_lims2 = 0.5/c/su*iti0k0(su*lims2[dyds_0_inds])[1]
-				ints_dyd_0 = ints_dyd_0_lims2 - ints_dyd_0_lims1
-				ans[dyds_0_inds] = ints_dyd_0
-			# calculate for nonzero dyds:
-			if len(dyds_nnz_inds) > 0:
-				if int_type == "quad":
-					g = lambda x: quad(self.frac_source_func, x[0], x[1], args=(x[2], su))[0]
-				elif int_type == "fixed_quad":
-					g = lambda x: fixed_quad(self.frac_source_func, x[0], x[1], args=(x[2], su), n=npoints)[0]
-				else:
-					raise ArgumentError("int type should be 'quad' or 'fixed_quad'")
-				nnz_items = np.hstack([lims1[dyds_nnz_inds].reshape(-1,1), lims2[dyds_nnz_inds].reshape(-1,1), dyds[dyds_nnz_inds].reshape(-1,1)])
-				ans[dyds_nnz_inds] = np.apply_along_axis(g, 1, nnz_items)
+			if self.well.boundaries == "infinite":
+				dyds_0_inds = np.argwhere(dyds==0.).flatten()
+				dyds_nnz_inds = np.argwhere(dyds!=0.).flatten()
+				# calcculate for zero dyds:
+				if len(dyds_0_inds) > 0:
+					ints_dyd_0_lims1 = 0.5/c/su*iti0k0(su*lims1[dyds_0_inds])[1]
+					ints_dyd_0_lims2 = 0.5/c/su*iti0k0(su*lims2[dyds_0_inds])[1]
+					ints_dyd_0 = ints_dyd_0_lims2 - ints_dyd_0_lims1
+					ans[dyds_0_inds] = ints_dyd_0
+				# calculate for nonzero dyds:
+				if len(dyds_nnz_inds) > 0:
+					if int_type == "quad":
+						g = lambda x: quad(self.frac_source_func, x[0], x[1], args=(x[2], su))[0]
+					elif int_type == "fixed_quad":
+						g = lambda x: fixed_quad(self.frac_source_func, x[0], x[1], args=(x[2], su), n=npoints)[0]
+					else:
+						raise ArgumentError("int type should be 'quad' or 'fixed_quad'")
+					nnz_items = np.hstack([lims1[dyds_nnz_inds].reshape(-1,1), lims2[dyds_nnz_inds].reshape(-1,1), dyds[dyds_nnz_inds].reshape(-1,1)])
+					ans[dyds_nnz_inds] = np.apply_along_axis(g, 1, nnz_items)
+			elif self.well.boundaries == "nnnn":
+				raise NotImplementedError
 		else:
 			raise NotImplementedError
 		return ans
